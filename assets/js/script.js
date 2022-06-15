9c100afe153787241fefec209a082c39b713d39f
Devon <devondgray28@gmail.com>
2022-06-15 04:29:46 -0500

    fixed initial map

--- assets/js/script.js
+++ assets/js/script.js
@@ -244,4 +244,16 @@ function initMap() {
   });
 }
 
-window.initMap = initMap;
+function initGoogle () {
+  var location = {
+    lat: 40.000,
+    lng: -79.000
+  }
+  var options = {
+    center: location,
+    zoom: 9
+  }
+  map = new google.maps.Map(document.getElementById("map"), options)
+}
+  
+
